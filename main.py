3d3d6e6cf5a02d491ac1e69f64346c6d2124f282
Farshid Nooshi <farshidnooshi726@aut.ac.ir>
2021-12-31 19:10:00 +0330

    done

--- main.py
+++ main.py
@@ -2,22 +2,23 @@
 # -*- coding: utf-8 -*-
 
 # project imports
+from MyControllerFinal import MyFuzzyControllerFinal
 from conf import ConfigReader
 from myController import MyFuzzyController
 from world import World
-from controller2 import FuzzyController2
 from manager import Manager
 
 
 conf = ConfigReader()
 
 if __name__ == '__main__':
+
     # world = World(**conf.world_config())
-    # controller = FuzzyController2()
+    # controller = MyFuzzyController()
     # manager = Manager(world, controller, **conf.simulation_config())
     # manager.run()
 
     world = World(**conf.world_config())
-    controller = MyFuzzyController()
+    controller = MyFuzzyControllerFinal()
     manager = Manager(world, controller, **conf.simulation_config())
     manager.run()
